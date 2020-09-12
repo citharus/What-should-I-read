0f8b0ec0af9c974e406d5e5fa8b6264c02d38348
citharus <derangler2004@gmail.com>
2020-09-12 13:01:53 +0200

    Removed specific number input

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from json import load
 
 
 class RandomBook:
-	def __init__(self, book_list: str, number: int = None):
+	def __init__(self, book_list: str):
 		self.book_list: str = book_list
-		self.number: int = number
 
 	def random(self):
-		if not self.number:
-			self.number = randint(0, 100)
+		number = randint(0, 100)
 
 		with open(self.book_list) as bl:
 			book_list: dict = load(bl)
-			book: dict = book_list[str(self.number)]
+			book: dict = book_list[str(number)]
 			return book
